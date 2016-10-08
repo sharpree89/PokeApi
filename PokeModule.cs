fc8d313ce9c39e1a4223f71eeb15a6b608da12ac
Preeya Sharma <sharpree89@gmail.com>
2016-10-08 09:51:09 -0700

    first commit

--- PokeModule.cs
+++ PokeModule.cs
@@ -13,12 +13,11 @@ namespace PokeInfo
             {
                 string name = "";
                 object species = "";
-                long exp = 0;
                 long weight = 0;
                 long height = 0;
+                long exp = 0;
 
-                // Our anonymous function is a parameter of type Action that returns a Dictionary
-                await WebRequest.SendRequest("http://pokeapi.co/api/v2/pokemon/1", new Action<Dictionary<string, object>>( JsonResponse =>
+                await WebRequest.SendRequest("http://pokeapi.co/api/v2/pokemon/7", new Action<Dictionary<string, object>>( JsonResponse =>
                     {
                         name = (string)JsonResponse["name"];
                         species = (object)JsonResponse["species"];
@@ -27,18 +26,14 @@ namespace PokeInfo
                         exp = (long)JsonResponse["base_experience"];
                        
                         @ViewBag.name = name;
+                        @ViewBag.species = species;
                         @ViewBag.weight = weight;
                         @ViewBag.height = height;
-                        @ViewBag.species = species;
                         @ViewBag.exp = exp;
-                       
                     }
                 ));
                 return View["poke.sshtml"];
             }); 
-            //trying to push
-       
-
         }
     }
 }
